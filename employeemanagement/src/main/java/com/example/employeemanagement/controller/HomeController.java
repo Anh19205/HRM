e1253871d4d489973e8a2399802eebd273698298
NguyenDacHao <haond5382@ut.edu.vn>
2026-01-09 21:26:44 +0700

    Initial commit

---
+++ employeemanagement/src/main/java/com/example/employeemanagement/controller/HomeController.java
@@ -0,0 +1,61 @@
+package com.example.employeemanagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Landingpage"; // templates/index.html
+    }
+     @GetMapping("/employees")
+    public String employeeList() {
+        return "EmployeeList";   // -> templates/EmployeeList.html
+    }
+
+    @GetMapping("/employee-form")
+    public String employeeForm() {
+        return "EmployeeForm";   // -> templates/EmployeeForm.html
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // templates/login.html
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "Dashboard"; // templates/dashboard.html
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register"; // templates/register.html
+    }
+    
+
+    @GetMapping("/verifyUsername")
+    public String verifyUsername() {
+        return "verifyUsername"; // templates/verify-username.html
+    }
+
+    @GetMapping("/resetPassword")
+    public String resetPassword() {
+        return "resetPassword"; // templates/reset-password.html
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile"; // templates/profile.html
+    }
+    @GetMapping("/departments")
+    public String departments() {
+        return "DepartmentList"; // templates/departments.html
+    }
+    @GetMapping("/department-form")
+    public String departmentForm() {
+        return "DepartmentForm";   // -> templates/DepartmentForm.html
+    }
+}
